760c611e3118a4a2a80345f89014f1b8ba2db935
Nicolas Long <nicolaslong@gmail.com>
2014-06-17 19:31:24 +0100

    Stash commit

--- src/main/scala/net/room271/HttpParser.scala
+++ src/main/scala/net/room271/HttpParser.scala
@@ -1,11 +1,53 @@
 package net.room271
 
-import java.io.InputStream
-import java.security.cert.X509Certificate
+import akka.util.ByteString
+import scala.util.parsing.combinator.RegexParsers
 
-object HttpParser {
+object HttpParser extends RegexParsers {
 
-  def parse(input: InputStream): Request = ???
+  val stringLiteral = (s"[^$CRLF]").r
+  val nonSpaceString = "[^ ]*".r
+  val CRLF = "\r\n"
+
+  // See: http://www.w3.org/Protocols/rfc2616/rfc2616-sec5.html#sec5
+  //
+  // An example request:
+  //   GET / HTTP/1.1
+  //   User-Agent: curl/7.30.0
+  //   Host: localhost:9000
+  //   Accept: */*
+  //
+  // Generally CRLF (escaped in Scala as: '\r\n') is used to separate parts
+  def parse(input: ByteString): Request = {
+    input.to
+
+
+    ???
+  }
+
+  def request: Parser[Request] = requestLine ~ CRLF ~ opt(headers) ~ CRLF ~ opt(body)
+
+  def requestLine: Parser[RequestLine] = method ~ uri ~ version
+
+  def method: Parser[RequestMethod] = get | post
+
+  def get: Parser[Get] = "GET" ^^ { _ => Get }
+
+  def post: Parser[Post] = "POST" ^^ { _ => Post }
+
+  def uri: Parser[String] = nonSpaceString
+
+  def version: Parser[String] = nonSpaceString
+
+  def headers: Parser[Map[String, String]] = rep(header) ^^ { _.toMap }
+
+  def header: Parser[(String, String)] = nonSpaceString ~ ":" ~ nonSpaceString ^^ {
+    case key ~ _ ~ value => (key, value)
+  }
+
+  def body: Parser[String] =
+
+  case class RequestLine(method: RequestMethod, uri: String, version: String)
 }
 
 
--- src/main/scala/net/room271/Models.scala
+++ src/main/scala/net/room271/Models.scala
@@ -1,6 +1,5 @@
 package net.room271
 
-import java.io.InputStream
 import java.security.cert.X509Certificate
 
 case class Request(
@@ -13,23 +12,23 @@ case class Request(
   requestMethod: RequestMethod,
   sslClientCert: Option[X509Certificate],
   headers: Map[String, String],
-  body: Option[InputStream])
+  body: Option[String])
 
 case class Response(
   status: Int,
   headers: Map[String, String],
-  body: Option[InputStream])
+  body: Option[String])
 
 sealed trait RequestMethod
-case object Get
-case object Head
-case object Post
-case object Put
-case object Delete
-case object Trace
-case object Options
-case object Patch
+case object Get extends RequestMethod
+case object Head extends RequestMethod
+case object Post extends RequestMethod
+case object Put extends RequestMethod
+case object Delete extends RequestMethod
+case object Trace extends RequestMethod
+case object Options extends RequestMethod
+case object Patch extends RequestMethod
 
 sealed trait Scheme
-case object Http
-case object Https
+case object Http extends Scheme
+case object Https extends Scheme
